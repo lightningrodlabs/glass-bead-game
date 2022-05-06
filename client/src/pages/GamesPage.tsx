f14c257716e0b39a7dfef249e320c0fb66b02ae5
jhweir <james@weco.io>
2022-05-06 16:05:43 +0100

    Sample game list set up

--- client/src/pages/GamesPage.tsx
+++ client/src/pages/GamesPage.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
-import styles from '@styles/pages/HomePage.module.scss'
+import React, { useState, useEffect } from 'react'
+import styles from '@styles/pages/GamesPage.module.scss'
+import Column from '@components/Column'
+import Row from '@components/Row'
+import Button from '@components/Button'
+
+const sampleGames = [
+    {
+        id: 1,
+        topic: 'Topic 1',
+        introDuration: 10,
+        numberOfTurns: 5,
+        moveDuration: 30,
+        intervalDuration: 0,
+        outroDuration: 30
+    },
+    {
+        id: 2,
+        topic: 'Topic 2',
+        introDuration: 10,
+        numberOfTurns: 5,
+        moveDuration: 30,
+        intervalDuration: 0,
+        outroDuration: 30
+    },
+    {
+        id: 3,
+        topic: 'Topic 3',
+        introDuration: 10,
+        numberOfTurns: 5,
+        moveDuration: 30,
+        intervalDuration: 0,
+        outroDuration: 30
+    }
+]
 
 const GamesPage = (): JSX.Element => {
+    const [games, setGames] = useState<any[]>(sampleGames)
+
+    function openGame(gameId) {
+        console.log('Open game: ', gameId)
+    }
+
+    useEffect(() => {
+        console.log('Get games!')
+        // backendShim
+        //     .getGames()
+        //     .then((games) => setGames(games))
+        //     .catch((error) => console.log(error))
+    }, [])
+
     return (
-        <div className={styles.wrapper}>
-            <p>games...</p>
-        </div>
+        <Column centerX centerY className={styles.wrapper}>
+            {games.map((game) => (
+                <Row key={game.id} spaceBetween className={styles.game}>
+                    <Column>
+                        <p>Id: {game.id}</p>
+                        <p>Topic: {game.topic}</p>
+                    </Column>
+                    <Button color='blue' text='Open game' onClick={() => openGame(game.id)} />
+                </Row>
+            ))}
+        </Column>
     )
 }
 
