cfb89d2d84ffb300ed943ec8cc6b7c9ae5b431d1
Brandon Norgaard <brandon@enlightenedworldview.com>
2022-05-09 17:49:58 -0700

    Changes to get the GUI up - not working at this point though

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -21,25 +21,25 @@ import config from './Config'
 import './index.css';
 
 // TODO: maybe this isn't necessary for GBG as standalone app and we could call into a weco library to do this if we are in that context?
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <>
-//       <BrowserRouter>
-//         <AccountContextProvider>
-//           <GoogleReCaptchaProvider reCaptchaKey={config.recaptchaSiteKey}>
-//             <NavBar />
-//             <Modals />
-//             <Routes>
-//               <Route path="/" element={<Home/>} />
-//               <Route path="/about" element={<About/>} />
-//             </Routes>
-//           </GoogleReCaptchaProvider>
-//         </AccountContextProvider>
-//       </BrowserRouter>
-//     </>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <>
+      <BrowserRouter>
+        {/* <AccountContextProvider> */}
+          <GoogleReCaptchaProvider reCaptchaKey={config.recaptchaSiteKey}>
+            <NavBar />
+            <Modals />
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/about" element={<About/>} />
+            </Routes>
+          </GoogleReCaptchaProvider>
+        {/* </AccountContextProvider> */}
+      </BrowserRouter>
+    </>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
