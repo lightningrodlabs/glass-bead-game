e22d59167b9558da17e3a31e97df86343b98b688
jhweir <james@weco.io>
2022-05-17 15:22:57 +0100

    navbar and routes re-added to App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
-import HomePage from '@pages/HomePage'
 import styles from '@styles/App.module.scss'
+import Row from '@components/Row'
+import HomePage from '@pages/HomePage'
+import GamesPage from '@pages/GamesPage'
+import GamePage from '@pages/GamePage'
 
 const App = (): JSX.Element => {
-    // todo: create <ContextProviders> component to wrap all contexts
-
     return (
         <div className={styles.wrapper}>
             <BrowserRouter history={createBrowserHistory}>
+                <Row centerX centerY className={styles.navbar}>
+                    <Link to='/'>
+                        <p>Home</p>
+                    </Link>
+                    <Link to='/games'>
+                        <p>Games</p>
+                    </Link>
+                </Row>
                 <Switch>
                     <Route path='/' exact component={HomePage} />
+                    <Route path='/games' exact component={GamesPage} />
+                    <Route path='/game/:gameId' component={GamePage} />
                 </Switch>
             </BrowserRouter>
         </div>
     )
 }
 
-export default App;
+export default App
