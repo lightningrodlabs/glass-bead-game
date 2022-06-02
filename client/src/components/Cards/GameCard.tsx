9de09a144a28e0f6b2452fe9983a147c2b3df160
jhweir <james@weco.io>
2022-06-02 16:18:27 +0100

    GameCard component set up

---
+++ client/src/components/Cards/GameCard.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import styles from '@styles/components/cards/GameCard.module.scss'
+import Column from '@src/components/Column'
+import Row from '@src/components/Row'
+import Button from '@components/Button'
+
+const GameCard = (props: { data: any }): JSX.Element => {
+    const { data } = props
+    const history = useHistory()
+    return (
+        <Column className={styles.wrapper}>
+            <Row style={{ marginBottom: 20 }}>
+                {data.game.topicImageUrl && <img src={data.game.topicImageUrl} alt='' />}
+                <Column>
+                    <h1>{data.game.topic}</h1>
+                    <p>{data.game.description}</p>
+                </Column>
+            </Row>
+            <Button
+                color='blue'
+                text='Open game'
+                onClick={() => history.push(`/game/${data.entryHash}`)}
+                style={{ width: 120, marginBottom: 20 }}
+            />
+            <p className='grey'>EntryHash: {data.entryHash}</p>
+        </Column>
+    )
+}
+
+export default GameCard
