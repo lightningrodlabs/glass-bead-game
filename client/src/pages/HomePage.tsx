b442aef16d8230a6eee6755d50e16be00a1e2b37
jhweir <james@weco.io>
2022-11-25 16:24:30 +0000

    Homepage updates

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -4,15 +4,23 @@ import GlassBeadGameService from '@src/glassbeadgame.service'
 import styles from '@styles/pages/HomePage.module.scss'
 import Column from '@components/Column'
 import Button from '@components/Button'
+import PlayerDetailsModal from '@components/Modals/PlayerDetailsModal'
 import CreateGameModal from '@components/Modals/CreateGameModal'
 import GameCard from '@components/Cards/GameCard'
 import { ReactComponent as CastaliaIcon } from '@svgs/castalia-logo.svg'
+import { ReactComponent as EditIcon } from '@svgs/edit-solid.svg'
 import { AppWebsocket, InstalledCell } from '@holochain/client'
+import LoadingWheel from '@src/components/LoadingWheel'
+import FlagImage from '@src/components/FlagImage'
+import Row from '@src/components/Row'
 
 const Homepage = (): JSX.Element => {
     const [gbgService, setGbgService] = useState<null | GlassBeadGameService>(null)
+    const [player, setPlayer] = useState<any>(null)
     const [games, setGames] = useState<any[]>([])
+    const [playerDetailsModalOpen, setPlayerDetailsModalOpen] = useState(false)
     const [createGameModalOpen, setCreateGameModalOpen] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     async function initialiseGBGService() {
         const client = await AppWebsocket.connect(`ws://localhost:${process.env.REACT_APP_HC_PORT}`)
@@ -21,9 +29,7 @@ const Homepage = (): JSX.Element => {
         const cellData = appInfo.cell_data.find(
             (c: InstalledCell) => c.role_id === 'glassbeadgame-role'
         )
-
         if (!cellData) throw new Error('No cell with glassbeadgame-role role id was found')
-
         const cellClient = new CellClient(holochainClient, cellData)
         setGbgService(new GlassBeadGameService(cellClient))
     }
@@ -33,11 +39,18 @@ const Homepage = (): JSX.Element => {
     }, [])
 
     useEffect(() => {
-        if (gbgService)
-            gbgService
-                .getGames()
-                .then((res) => setGames(res))
+        if (gbgService) {
+            Promise.all([
+                gbgService.getPlayerDetails(gbgService.myAgentPubKey),
+                gbgService.getGames(),
+            ])
+                .then((data) => {
+                    setPlayer(data[0])
+                    setGames(data[1])
+                    setLoading(false)
+                })
                 .catch((error) => console.log(error))
+        }
     }, [gbgService])
 
     return (
@@ -45,20 +58,64 @@ const Homepage = (): JSX.Element => {
             <Column centerX centerY className={styles.gbgIcon}>
                 <CastaliaIcon />
             </Column>
-            <Button color='blue' text='Create game' onClick={() => setCreateGameModalOpen(true)} />
-            {createGameModalOpen && (
-                <CreateGameModal
-                    gbgService={gbgService}
-                    games={games}
-                    setGames={setGames}
-                    close={() => setCreateGameModalOpen(false)}
-                />
+            {loading ? (
+                <LoadingWheel />
+            ) : (
+                <Column>
+                    {!player ? (
+                        <Button
+                            color='purple'
+                            text='Add player details'
+                            onClick={() => setPlayerDetailsModalOpen(true)}
+                        />
+                    ) : (
+                        <Column centerX>
+                            <Row centerY style={{ marginBottom: 30 }}>
+                                <FlagImage
+                                    type='user'
+                                    size={50}
+                                    imagePath={player.image}
+                                    style={{ marginRight: 15 }}
+                                />
+                                <p className={styles.playerName}>{player.name}</p>
+                                <button
+                                    type='button'
+                                    onClick={() => setPlayerDetailsModalOpen(true)}
+                                    className={styles.editPlayerDetailsButton}
+                                >
+                                    <EditIcon />
+                                </button>
+                            </Row>
+                            <Button
+                                color='blue'
+                                text='Create game'
+                                onClick={() => setCreateGameModalOpen(true)}
+                            />
+                            <Column centerX centerY className={styles.games}>
+                                {games.map((data) => (
+                                    <GameCard key={data.entryHash} data={data} />
+                                ))}
+                            </Column>
+                        </Column>
+                    )}
+                    {gbgService && playerDetailsModalOpen && (
+                        <PlayerDetailsModal
+                            gbgService={gbgService}
+                            player={player}
+                            setPlayer={setPlayer}
+                            close={() => setPlayerDetailsModalOpen(false)}
+                        />
+                    )}
+                    {gbgService && createGameModalOpen && (
+                        <CreateGameModal
+                            gbgService={gbgService}
+                            games={games}
+                            setGames={setGames}
+                            close={() => setCreateGameModalOpen(false)}
+                        />
+                    )}
+                </Column>
             )}
-            <Column centerX centerY className={styles.games}>
-                {games.map((data) => (
-                    <GameCard key={data.entryHash} data={data} />
-                ))}
-            </Column>
         </Column>
     )
 }
